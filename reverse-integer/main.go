e9f7959cd53a666e73ee9300527c1ae6e9812c2e
nickolasgough <nickolas.v.gough@gmail.com>
2024-10-28 21:49:54 +0000

    Upload solution to reverse integer.

---
+++ reverse-integer/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"strconv"
+)
+
+// https://leetcode.com/problems/reverse-integer/
+
+func reverse(x int) int {
+	sign := ""
+	maxN := "2147483647"
+	xStr := strconv.Itoa(x)
+	if x < 0 {
+		sign = "-"
+		maxN = "2147483648"
+		xStr = xStr[1:]
+	}
+
+	maxLen := len(maxN)
+	xLen := len(xStr)
+	result := make([]rune, xLen)
+	for i := xLen - 1; i >= 0; i -= 1 {
+		result[xLen-1-i] = rune(xStr[i])
+	}
+	resultStr := string(result)
+	if xLen >= maxLen && resultStr > maxN {
+		return 0
+	}
+	resultStr = sign + resultStr
+	resultInt, _ := strconv.Atoi(resultStr)
+	return resultInt
+}
