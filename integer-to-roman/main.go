b248997e5b88b96111cb4db8d49510afe836817f
nickolasgough <nickolas.v.gough@gmail.com>
2024-10-29 22:01:49 +0000

    Upload solution for roman to integer.

--- integer-to-roman/main.go
+++ integer-to-roman/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"strconv"
+)
+
 // https://leetcode.com/problems/integer-to-roman/
 
 type romanNumeral struct {
@@ -9,46 +13,44 @@ type romanNumeral struct {
 
 func intToRoman(n int) string {
 	resultStr := ""
-	remainder := n
+	nStr := strconv.Itoa(n)
+	nLen := len(nStr)
 	romanNumerals := []*romanNumeral{
-		{Letter: "M", Value: 1000},
-		{Letter: "D", Value: 500},
-		{Letter: "C", Value: 100},
-		{Letter: "L", Value: 50},
-		{Letter: "X", Value: 10},
-		{Letter: "V", Value: 5},
 		{Letter: "I", Value: 1},
+		{Letter: "V", Value: 5},
+		{Letter: "X", Value: 10},
+		{Letter: "L", Value: 50},
+		{Letter: "C", Value: 100},
+		{Letter: "D", Value: 500},
+		{Letter: "M", Value: 1000},
 	}
-	for i, rn := range romanNumerals {
-		isDivisor := i%2 == 0
-		if !isDivisor {
+	for i := 0; i < nLen; i += 1 {
+		r := 2 * i
+		c := nStr[nLen-1-i]
+		if c == '0' {
 			continue
 		}
-
-		nLetters := int(remainder / rn.Value)
-		if nLetters > 0 {
-			remainder = remainder % rn.Value
-
-			lettersToAdd := ""
-			if nLetters == 4 {
-				otherRn := romanNumerals[i-1]
-				lettersToAdd = rn.Letter + otherRn.Letter
-			} else if nLetters == 9 {
-				otherRn := romanNumerals[i-2]
-				lettersToAdd = rn.Letter + otherRn.Letter
-			} else if nLetters >= 5 {
-				otherRn := romanNumerals[i-1]
-				lettersToAdd += otherRn.Letter
-				for range nLetters - 5 {
-					lettersToAdd += rn.Letter
-				}
-			} else {
-				for range nLetters {
-					resultStr += rn.Letter
-				}
+		cInt, _ := strconv.Atoi(string(c))
+		rn := romanNumerals[r]
+		lettersToAdd := ""
+		if cInt == 4 {
+			nextRn := romanNumerals[r+1]
+			lettersToAdd = rn.Letter + nextRn.Letter
+		} else if cInt == 9 {
+			nextRn := romanNumerals[r+2]
+			lettersToAdd = rn.Letter + nextRn.Letter
+		} else if cInt >= 5 {
+			nextRn := romanNumerals[r+1]
+			lettersToAdd += nextRn.Letter
+			for range cInt - 5 {
+				lettersToAdd += rn.Letter
 			}
-			resultStr += lettersToAdd
+		} else {
+			for range cInt {
+				lettersToAdd += rn.Letter
+			}
 		}
+		resultStr = lettersToAdd + resultStr
 	}
 	return resultStr
 }
---
+++ roman-to-integer/main.go
@@ -0,0 +1,37 @@
+package main
+
+// https://leetcode.com/problems/roman-to-integer/
+
+type romanNumeral struct {
+	Letter  rune
+	Value   int
+	OtherRn rune
+}
+
+func romanToInt(s string) int {
+	sLen := len(s)
+	resultInt := 0
+	romanNumerals := map[rune]*romanNumeral{
+		'I': {Letter: 'I', Value: 1},
+		'V': {Letter: 'V', Value: 5, OtherRn: 'I'},
+		'X': {Letter: 'X', Value: 10, OtherRn: 'I'},
+		'L': {Letter: 'L', Value: 50, OtherRn: 'X'},
+		'C': {Letter: 'C', Value: 100, OtherRn: 'X'},
+		'D': {Letter: 'D', Value: 500, OtherRn: 'C'},
+		'M': {Letter: 'M', Value: 1000, OtherRn: 'C'},
+	}
+	for i := sLen - 1; i >= 0; i -= 1 {
+		c := rune(s[i])
+		rn := romanNumerals[c]
+		resultInt += rn.Value
+
+		if rn.OtherRn != 0 && i > 0 {
+			otherRn := romanNumerals[rn.OtherRn]
+			if rune(s[i-1]) == otherRn.Letter {
+				i -= 1
+				resultInt -= otherRn.Value
+			}
+		}
+	}
+	return resultInt
+}
